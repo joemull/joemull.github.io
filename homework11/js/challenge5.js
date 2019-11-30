f3d0d9bcee9815900e5eac44503ef155c8ef6f05
Joseph Muller <47217308+joemull@users.noreply.github.com>
2019-11-30 12:53:01 -0500

    Delete commented code

--- homework11/js/challenge5.js
+++ homework11/js/challenge5.js
@@ -29,42 +29,4 @@ $(document).ready(function(event){
 	});
 
 });
-
-// $(document).ready(function(){
-// 	$('img').mouseover(function(){
-// 		$('#image').css('backgroundImage',
-// 			"url('"+ $(this).attr('src') +"')");
-// 		$('#image').attr('alt',$(this).attr('alt'));
-// 		$('#image p').HTML($(this).attr('alt'));
-// 	});
-// 	$('img').mouseleave(function(){
-// 		$('#image').css('backgroundImage',"url('')");
-// 		$('#image').attr('alt','');
-// 		$('#image p').HTML('');
-// 	});
-// });
-
-// console.log("here")
-// var imgs = document.querySelectorAll("img");
-// var msg = "Hover over an image below."
-// for (let i = 0; i < imgs.length;i++){
-// 	imgs[i].onmouseover = function() {
-// 	 	document.querySelector('#image').style.backgroundImage="url('"+imgs[i].src+"')";
-// 	 	document.querySelector('#image').innerHTML= imgs[i].alt;
-// 	 }
-//
-// 	 imgs[i].onfocus = function() {
-// 	 	document.querySelector('#image').style.backgroundImage="url('"+imgs[i].src+"')";
-// 	 	document.querySelector('#image').innerHTML= imgs[i].alt;
-// 	 }
-//
-// 	 imgs[i].onmouseleave = function() {
-// 	 	document.querySelector('#image').style.backgroundImage="url('')";
-// 	 	document.querySelector('#image').innerHTML= msg;
-// 	 }
-//
-// 	 imgs[i].onblur = function() {
-// 	 	document.querySelector('#image').style.backgroundImage="url('')";
-// 	 	document.querySelector('#image').innerHTML= msg;
-// 	 }
-// }
+	
